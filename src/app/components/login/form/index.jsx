8c268c960cfba1f837492c2dbfdf2725fce43c13
Sriram Sridharan <s.sriram@yethi.in>
2024-02-08 11:22:18 +0530

    Implement Login Form

--- src/app/components/login/form/index.jsx
+++ src/app/components/login/form/index.jsx
@@ -3,24 +3,49 @@
 import Button from "@/components/shared/button";
 import Input from "@/components/shared/input";
 import { useState } from "react";
-import { useFormState } from "react-dom";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
-  const [state, formAction] = useFormState(() => {}, {});
-  //   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userId = e.currentTarget.userId.value;
+    const password = e.currentTarget.password.value;
+    if (!userId || !password) {
+      return;
+    }
+    setLoading(true);
+    signIn("credentials", {
+      redirect: false,
+      userId,
+      password,
+    }).then(({ error }) => {
+      if (error) {
+        setError(error);
+        setLoading(false);
+      } else {
+        router.refresh();
+        router.push("/");
+      }
+    });
+  };
 
   return (
     <div className="p-4 border rounded-lg">
-      <form name="login_form" id="login_form">
+      <form onSubmit={handleSubmit} name="login_form" id="login_form">
         <div className="grid grid-cols-12">
           <div className="my-3 col-span-12 flex items-center justify-center">
             <h2 className="text-base font-semibold underline">
               Login with your credentials
             </h2>
           </div>
-          {!state.success && (
+          {error && (
             <div className="col-span-12 my-3">
-              <h3 className="text-sm text-tenjin-error">{state.message}</h3>
+              <h3 className="text-sm text-tenjin-error">{error}</h3>
             </div>
           )}
           <div className="col-span-12">
@@ -40,7 +65,7 @@ const LoginForm = () => {
               Reset
             </Button>
             <Button
-              //   loading={loading}
+              loading={loading}
               loadingText="Please Wait..."
               type="submit"
               size="small"
