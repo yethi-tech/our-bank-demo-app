6034e5975b30a6ce5eef02ad9e9dfee7145cf990
Sriram Srihdaran <s.sriram@yethi.in>
2024-04-30 16:41:57 +0530

    Reload page after auth successful popup is closed

--- src/app/components/customers/authorizeCustomerButton/index.jsx
+++ src/app/components/customers/authorizeCustomerButton/index.jsx
@@ -4,15 +4,27 @@ import Button from "@/components/shared/button";
 import React, { useState } from "react";
 import { GrUserAdmin } from "react-icons/gr";
 import AlertDialog from "../../alertDialog";
+import { useRouter } from "next/navigation";
 
 const AuthorizeCustomerButton = ({ id }) => {
+  const router = useRouter();
+
   const [authorizationResult, setAuthorizationResult] = useState(null);
   const [loading, setLoading] = useState(false);
+
   const doAuthorize = async () => {
     setLoading(true);
     const result = await authorizeCustomer(id);
     setAuthorizationResult(result);
     setLoading(false);
+  };
+
+  const handleAlertClose = () => {
+    if (authorizationResult.success) {
+      router.refresh();
+    } else {
+      setAuthorizationResult(null);
+    }
   };
 
   return (
@@ -38,7 +50,7 @@ const AuthorizeCustomerButton = ({ id }) => {
               ? "Authorization successful!"
               : authorizationResult.message
           }
-          onClose={() => setAuthorizationResult(null)}
+          onClose={handleAlertClose}
         />
       )}
     </>
