acd2b82fca77a1b82fd0795f031c55e907b51c91
Sriram Sridharan <s.sriram@yethi.in>
2024-05-07 12:33:23 +0530

    Modified some locators for testing purpose

--- src/app/components/tasks/form/index.jsx
+++ src/app/components/tasks/form/index.jsx
@@ -69,7 +69,7 @@ const AddForm = ({ onCancel }) => {
 
                 <div className="col-span-full">
                   <label
-                    htmlFor="taskSummary"
+                    htmlFor="task_summary"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
                     Task Summary
@@ -79,7 +79,7 @@ const AddForm = ({ onCancel }) => {
                       <input
                         type="text"
                         name="taskSummary"
-                        id="taskSummary"
+                        id="task_summary"
                         autoComplete="none"
                         className="block  flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                         placeholder="Summarize your task"
@@ -92,7 +92,7 @@ const AddForm = ({ onCancel }) => {
                     htmlFor="user"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    User
+                    Assigned To (enter name)
                   </label>
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 w-full">
